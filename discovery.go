08a44082e7c1b2a34b4afe19c34fc11b1fec90a2
Florian Lenz <flenz81@googlemail.com>
2018-03-22 11:03:27 +0100

    [discovery] removed discoverd method from interface

--- discovery.go
+++ discovery.go
@@ -4,15 +4,15 @@ import (
 	peerstore "github.com/libp2p/go-libp2p-peerstore"
 )
 
-type DiscoveryCallback = func(info peerstore.PeerInfo)
+type DiscoveryCallback = func(err error, info *peerstore.PeerInfo)
 
 //Interface for discovery mechanism
 //In case you want to add a new one
 //make sure to implement this interface
 type DiscoveryMechanism interface {
-	//Start discovery
-	Start()
+	//Start discovery.
+	//cb will be called with discovered peer
+	Start(cb DiscoveryCallback)
 	//Stop discovery
 	Stop()
-	Discovered(cb DiscoveryCallback)
 }
